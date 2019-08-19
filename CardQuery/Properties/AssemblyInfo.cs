1fd1d629f5a56d0770bbde530664d08d8f9d20ac
Chuck Lu <chuck.lu@qq.com>
2019-08-19 18:56:48 +0800

    upgrade version to 2019.8.19.1

--- CardQuery/Properties/AssemblyInfo.cs
+++ CardQuery/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("CardQuery")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Chuck Lu")]
 [assembly: AssemblyProduct("CardQuery")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2019.8.19.1")]
+[assembly: AssemblyFileVersion("2019.8.19.1")]
