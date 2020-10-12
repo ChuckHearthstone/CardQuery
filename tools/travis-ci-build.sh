dc580940e8db4c70afee69371b77e0a61d6afea0
Chuck Lu <chuck.lu@qq.com>
2020-10-12 17:11:44 +0800

    update the folder name of source code

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Changing to /src directory..."
-cd SonarqubeTest
+cd CardQuery
 echo "Executing MSBuild.exe begin command..."
 mono ../tools/sonar/SonarScanner.MSBuild.exe begin /o:"chuckhearthstone" /k:"ChuckHearthstone_CardQuery" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
