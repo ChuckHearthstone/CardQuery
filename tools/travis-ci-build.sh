0f95216336aa965b60fad5bec77c97bfbef51a95
Chuck Lu <chuck.lu@qq.com>
2020-10-12 17:03:59 +0800

    update the solution file name in build script

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -4,6 +4,6 @@ cd SonarqubeTest
 echo "Executing MSBuild.exe begin command..."
 mono ../tools/sonar/SonarScanner.MSBuild.exe begin /o:"chuckhearthstone" /k:"ChuckHearthstone_CardQuery" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
-msbuild /p:Configuration=Release /t:Rebuild ../SonarqubeTest.sln
+msbuild /p:Configuration=Release /t:Rebuild ../CardQuery.sln
 echo "Executing MSBuild.exe end command..."
 mono ../tools/sonar/SonarScanner.MSBuild.exe end /d:sonar.login=${SONAR_TOKEN}
