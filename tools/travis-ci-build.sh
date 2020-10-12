a7f787eb8c4ba102a2c25303214e86a8ad907a98
Chuck Lu <chuck.lu@qq.com>
2020-10-12 16:56:23 +0800

    move organization and project key to build script

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -2,7 +2,7 @@
 echo "Changing to /src directory..."
 cd SonarqubeTest
 echo "Executing MSBuild.exe begin command..."
-mono ../tools/sonar/SonarScanner.MSBuild.exe begin /o:"chucktest" /k:"ChuckTest_SonarqubeTest" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+mono ../tools/sonar/SonarScanner.MSBuild.exe begin /o:"chuckhearthstone" /k:"ChuckHearthstone_CardQuery" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
 msbuild /p:Configuration=Release /t:Rebuild ../SonarqubeTest.sln
 echo "Executing MSBuild.exe end command..."
